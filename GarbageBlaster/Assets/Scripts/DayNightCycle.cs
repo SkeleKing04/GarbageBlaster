2b794605b19623b94e78e5da6349400a54af9c50
SkeleKing04 <jjlucas04@bigpond.com>
2021-03-29 12:46:44 +1030

    Started DayNightCycle

---
+++ GarbageBlaster/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public int CycleSpeed;
+    public GameObject m_OrbitCenter;
+    private int i = 0;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_OrbitCenter.transform.rotation = new Quaternion(i, 0f, 0f, 0f);
+        i++;
+    }
+}
