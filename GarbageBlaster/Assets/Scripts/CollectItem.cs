03d2f96f31752ca568395c44cc05b502d4408322
SkeleKing04 <jjlucas04@bigpond.com>
2021-03-30 10:20:52 +1030

    Started Item Collection Code

---
+++ GarbageBlaster/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{
+    public GameObject m_Collectable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GarbageBlaster/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject m_ItemToSpawn;
+    public GameObject[] m_SpawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
