464afa5a143dcb45141db9081a609b6f2d19fcdc
SkeleKing04 <jjlucas04@bigpond.com>
2021-03-29 17:31:16 +1030

    Made Goal

--- GarbageBlaster/Assets/Scripts/DayNightCycle.cs
+++ GarbageBlaster/Assets/Scripts/DayNightCycle.cs
@@ -1,26 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
     public GameObject m_CenterPoint;
     public int m_RotateAmount = 1;
+    private int rotateIncrease;
     // Start is called before the first frame update
     void Start()
     {
-        IEnumerator;
+        //m_RotateAmount = m_RotateAmount * -1;
+        rotateIncrease = m_RotateAmount;
     }
-
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Penis");
-        m_CenterPoint.transform.rotation = new Quaternion(m_RotateAmount, 0, 0, 1);
-    }
-    IEnumerator
-    {
-        m_CenterPoint.transform.rotation = new Quaternion(m_RotateAmount, 0, 0, 1);
-
-    }
+    void FixedUpdate()
+        {
+            Debug.Log(rotateIncrease);
+            m_CenterPoint.transform.rotation = new Quaternion(Time.deltaTime, 0, 0, rotateIncrease);
+            rotateIncrease -= m_RotateAmount;
+        }
 }
