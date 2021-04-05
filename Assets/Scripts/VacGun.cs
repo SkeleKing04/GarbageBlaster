f8041b415761583c6b458aaf87e2ee829c1708bf
SkeleKing04 <jjlucas04@bigpond.com>
2021-04-05 23:09:39 +0930

    Final Push from personal laptop

---
+++ Assets/Scripts/VacGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacGun : MonoBehaviour
+{
+    public Rigidbody m_GarbageBag;
+    public Transform m_FireTransform;
+    public float m_LaunchForce = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Fire();
+        }
+    }
+    private void Fire()
+    {
+        Rigidbody GarbageInstance = Instantiate(m_GarbageBag, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        GarbageInstance.velocity = m_LaunchForce * m_FireTransform.forward;
+    }
+}
