e5c559d3e5f94be088f29ae104fb87c1ce864c01
Jackson Lucas <jjlucas04@bigpond.com>
2021-05-07 12:45:36 +0930

    Added start menu and gamestates

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -31,42 +31,74 @@ public class GameManager : MonoBehaviour
     public float waitFor;
     //private bool wait = false;
     // Start is called before the first frame update
+    public enum GameState
+    {
+        Start,
+        Playing,
+        GameOver
+    };
+    private GameState m_GameState;
+    public GameState State { get { return m_GameState; } }
     void Start()
     {
         currentDirectory = Application.dataPath;
         Debug.Log("Directory - " + currentDirectory);
         m_SettingEditor.LoadSettings(m_VolumeSlider, m_FOVSlider);
         m_HighScoreEditor.LoadScores();
-        ChangeVolume();
-        ChangeFOV();
-        m_GameOver = false;
-        m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
-        VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
-        m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
-        foreach (GameObject gameObject in m_PauseMenu)
-        {
-            gameObject.gameObject.SetActive(false);
-        }
-        m_MessageText.gameObject.SetActive(false);
-        AudioListener.volume = m_VolumeSlider.value;
-        m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
-        m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
-        m_DisplayTime = m_LevelTime;
-        //m_MessageText.text = "Ready...";
-        //Time.timeScale = 0;
-        //waitFor = 3;
-        //StartCoroutine(WaitCommand(waitFor));
-        //WaitUntil waitUntil = wait == true;
-        //m_MessageText.text = "Go!";
-        //StartCoroutine(Timer(m_DisplayTime, m_TimerDisplay));
-        //waitFor = 0.5f;
-        //StartCoroutine(WaitCommand(waitFor));
-        m_MessageText.gameObject.SetActive(false);
-        //Time.timeScale = 1;
+        m_GameState = GameState.Start;
+        
+        
     }
     // Update is called once per frame
     void Update()
     {
+        switch (m_GameState)
+        {
+            case GameState.Start:
+                ChangeVolume();
+                ChangeFOV();
+                //m_GameOver = false;
+                m_GameState = GameState.Playing;
+                break;
+            case GameState.Playing:
+                m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
+                VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
+                m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
+                foreach (GameObject gameObject in m_PauseMenu)
+                {
+                    gameObject.gameObject.SetActive(false);
+                }
+                m_MessageText.gameObject.SetActive(false);
+                AudioListener.volume = m_VolumeSlider.value;
+                m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
+                m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
+                m_DisplayTime = m_LevelTime;
+                //m_MessageText.text = "Ready...";
+                //Time.timeScale = 0;
+                //waitFor = 3;
+                //StartCoroutine(WaitCommand(waitFor));
+                //WaitUntil waitUntil = wait == true;
+                //m_MessageText.text = "Go!";
+                //StartCoroutine(Timer(m_DisplayTime, m_TimerDisplay));
+                //waitFor = 0.5f;
+                //StartCoroutine(WaitCommand(waitFor));
+                m_MessageText.gameObject.SetActive(false);
+                //Time.timeScale = 1;
+                break;
+            case GameState.GameOver:
+                m_MessageText.gameObject.SetActive(true);
+                Debug.Log("Game Over");
+                PauseGame();
+                m_isGamePaused = false;
+                m_PauseMenu[1].SetActive(false);
+                m_MessageText.text = "Time's Up!";
+                //waitFor = 2;
+                //StartCoroutine(WaitCommand(waitFor));
+                m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
+                m_HighScoreEditor.AddScore(m_Score, m_PlayerName);
+                m_HighScoreEditor.SaveScores();
+                break;
+        }
         if (Input.GetKeyUp(KeyCode.Tab) && m_isGamePaused == false)
         {
             PauseGame();
@@ -149,7 +181,7 @@ public class GameManager : MonoBehaviour
         {
             while (m_GameOver == false)
             {
-                GameOver();
+                m_GameState = GameState.GameOver;
                 m_GameOver = true;
             }
         }
@@ -168,21 +200,6 @@ public class GameManager : MonoBehaviour
             }
         }
     }*/
-    public void GameOver()
-    {
-        m_MessageText.gameObject.SetActive(true);
-        Debug.Log("Game Over");
-        PauseGame();
-        m_isGamePaused = false;
-        m_PauseMenu[1].SetActive(false);
-        m_MessageText.text = "Time's Up!";
-        //waitFor = 2;
-        //StartCoroutine(WaitCommand(waitFor));
-        m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
-        m_HighScoreEditor.AddScore(m_Score , m_PlayerName);
-        m_HighScoreEditor.SaveScores();
-
-    }
     IEnumerator WaitCommand(float waitFor)
     {
         Debug.Log("Waiting for " + waitFor + " seconds");
@@ -190,5 +207,9 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("Done waiting");
 
+    }
+    public void StartPlaying()
+    {
+        m_GameState = GameState.Playing;
     }
 }
