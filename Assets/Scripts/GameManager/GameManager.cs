0fc1a7c8f18e500d76518271929b9c6591b198a7
SkeleKing04 <jjlucas04@bigpond.com>
2021-05-13 10:26:12 +0930

    Worked on game over scene

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
-using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -31,6 +28,7 @@ public class GameManager : MonoBehaviour
     string currentDirectory;
     public bool waiting = false;
     public float waitFor;
+    public bool GameStateStartUp;
     //private bool wait = false;
     // Start is called before the first frame update
     public enum GameState
@@ -41,7 +39,7 @@ public class GameManager : MonoBehaviour
         GameOver,
         Paused
     };
-    private GameState m_GameState;
+    public GameState m_GameState;
     public GameState State { get { return m_GameState; } }
     void Start()
     {
@@ -49,6 +47,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("Directory - " + currentDirectory);
         m_SettingEditor.LoadSettings(m_VolumeSlider, m_FOVSlider);
         m_HighScoreEditor.LoadScores();
+        GameStateStartUp = true;
         m_GameState = GameState.Start;
     }
     // Update is called once per frame
@@ -59,84 +58,106 @@ public class GameManager : MonoBehaviour
             //case GameState.Opening:
                 //break;
             case GameState.Start:
-                Time.timeScale = 0;
-                m_PauseMenu[0].gameObject.SetActive(true);
-                m_PauseMenu[2].gameObject.SetActive(true);
-                m_DisplayTime = m_LevelTime;
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-                foreach (Component part in m_PlayerParts)
+                while (GameStateStartUp == true)
                 {
-                    part.gameObject.SetActive(false);
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    foreach (GameObject gameObject in m_InGameHUD)
+                    {
+                        gameObject.gameObject.SetActive(false);
+                    }
+                    foreach (GameObject gameObject in m_PauseMenu)
+                    {
+                        gameObject.gameObject.SetActive(true);
+                    }
+                    m_PauseMenu[1].gameObject.SetActive(false);
+                    m_PauseMenu[3].gameObject.SetActive(false);
+                    Time.timeScale = 0;
+                    m_isGamePaused = true;
+                    foreach (Component part in m_PlayerParts)
+                    {
+                        part.gameObject.SetActive(false);
+                    }
+                    m_DisplayTime = m_LevelTime;
+                    GameStateStartUp = false;
                 }
                 break;
             case GameState.Playing:
-                ChangeVolume();
-                ChangeFOV();
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-                Time.timeScale = 1;
-                foreach (GameObject gameObject in m_InGameHUD)
+                while (GameStateStartUp == true)
                 {
-                    gameObject.gameObject.SetActive(true);
+                    ChangeVolume();
+                    ChangeFOV();
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    Time.timeScale = 1;
+                    foreach (GameObject gameObject in m_InGameHUD)
+                    {
+                        gameObject.gameObject.SetActive(true);
+                    }
+                    foreach (GameObject gameObject in m_PauseMenu)
+                    {
+                        gameObject.gameObject.SetActive(false);
+                    }
+                    foreach (Component part in m_PlayerParts)
+                    {
+                        part.gameObject.SetActive(true);
+                    }
+                    //m_GameOver = false;
+                    if (m_NameInput.text != null)
+                    {
+                        m_PlayerName = m_NameInput.text;
+                    }
+                    else
+                    {
+                        m_PlayerName = "Joe";
+                    }
+                    m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
+                    VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
+                    m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
+                    m_MessageText.gameObject.SetActive(false);
+                    AudioListener.volume = m_VolumeSlider.value;
+                    m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
+                    m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
+                    //m_MessageText.text = "Ready...";
+                    //Time.timeScale = 0;
+                    //waitFor = 3;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    //WaitUntil waitUntil = wait == true;
+                    //m_MessageText.text = "Go!";
+                    //StartCoroutine(Timer(m_DisplayTime, m_TimerDisplay));
+                    //waitFor = 0.5f;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    m_MessageText.gameObject.SetActive(false);
+                    //Time.timeScale = 1;
+                    GameStateStartUp = false;
                 }
-                foreach (GameObject gameObject in m_PauseMenu)
-                {
-                    gameObject.gameObject.SetActive(false);
-                }
-                foreach (Component part in m_PlayerParts)
-                {
-                    part.gameObject.SetActive(true);
-                }
-                //m_GameOver = false;
-                if (m_NameInput.text != null)
-                {
-                    m_PlayerName = m_NameInput.text;
-                }
-                else
-                {
-                    m_PlayerName = "Joe";
-                }
-                m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
-                VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
-                m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
-                m_MessageText.gameObject.SetActive(false);
-                AudioListener.volume = m_VolumeSlider.value;
-                m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
-                m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
-                //m_MessageText.text = "Ready...";
-                //Time.timeScale = 0;
-                //waitFor = 3;
-                //StartCoroutine(WaitCommand(waitFor));
-                //WaitUntil waitUntil = wait == true;
-                //m_MessageText.text = "Go!";
-                //StartCoroutine(Timer(m_DisplayTime, m_TimerDisplay));
-                //waitFor = 0.5f;
-                //StartCoroutine(WaitCommand(waitFor));
-                m_MessageText.gameObject.SetActive(false);
-                //Time.timeScale = 1;
                 if (Input.GetKeyUp(KeyCode.Escape) && m_isGamePaused == false)
                 {
                     PauseGame();
                 }
+                UpdateTimer();
                 break;
             case GameState.Paused:
                 break;
             case GameState.GameOver:
-                m_MessageText.gameObject.SetActive(true);
-                Debug.Log("Game Over");
-                PauseGame();
-                m_isGamePaused = false;
-                m_PauseMenu[1].SetActive(false);
-                m_MessageText.text = "Time's Up!";
-                //waitFor = 2;
-                //StartCoroutine(WaitCommand(waitFor));
-                m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
-                m_HighScoreEditor.AddScore(m_Score, m_PlayerName);
-                m_HighScoreEditor.SaveScores();
+                while (GameStateStartUp == true)
+                {
+                    m_MessageText.gameObject.SetActive(true);
+                    Debug.Log("Game Over");
+                    PauseGame();
+                    m_isGamePaused = false;
+                    m_PauseMenu[1].SetActive(false);
+                    m_PauseMenu[3].SetActive(true);
+                    m_MessageText.text = "Time's Up!";
+                    //waitFor = 2;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
+                    m_HighScoreEditor.AddScore(m_Score, m_PlayerName);
+                    m_HighScoreEditor.SaveScores();
+                    GameStateStartUp = false;
+                }
                 break;
         }
-        UpdateTimer();
     }
     public void UpdateScoreText()
     {
@@ -164,7 +185,9 @@ public class GameManager : MonoBehaviour
             {
                 gameObject.gameObject.SetActive(true);
             }
-            Time.timeScale = 0;
+        m_PauseMenu[2].gameObject.SetActive(false);
+        m_PauseMenu[3].gameObject.SetActive(false);
+        Time.timeScale = 0;
             m_isGamePaused = true;
         foreach (Component part in m_PlayerParts)
         {
@@ -210,6 +233,7 @@ public class GameManager : MonoBehaviour
         {
             //while (m_GameOver == false)
             //{
+            GameStateStartUp = true;
                 m_GameState = GameState.GameOver;
                 //m_GameOver = true;
             //}
@@ -239,6 +263,27 @@ public class GameManager : MonoBehaviour
     }
     public void StartPlaying()
     {
+        GameStateStartUp = true;
         m_GameState = GameState.Playing;
+    }
+    public void RestartGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        foreach (GameObject gameObject in m_InGameHUD)
+        {
+            gameObject.gameObject.SetActive(true);
+        }
+        foreach (GameObject gameObject in m_PauseMenu)
+        {
+            gameObject.gameObject.SetActive(false);
+        }
+        Time.timeScale = 1;
+        m_isGamePaused = false;
+        foreach (Component part in m_PlayerParts)
+        {
+            part.gameObject.SetActive(true);
+        }
+        m_SettingEditor.SaveSettings(m_VolumeSlider, m_FOVSlider);
     }
 }
