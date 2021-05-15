cbefde7dc323d62ba25385524d423365014e361d
SkeleKing04 <jjlucas04@bigpond.com>
2021-05-15 23:32:28 +0930

    added file causing errors

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,79 +1,183 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.IO;
-using System;
 
 public class GameManager : MonoBehaviour
 {
+    public highscoreEditor m_HighScoreEditor;
+    public settingEditor m_SettingEditor;
     public int m_Score = 0;
+    public string m_PlayerName;
     public Text m_ScoreDisplay;
     public Text m_GarbageLoadedDisplay;
     public GameObject[] m_InGameHUD;
     public GameObject[] m_PauseMenu;
     public bool m_isGamePaused = false;
+    public InputField m_NameInput;
     public Component[] m_PlayerParts;
+    public Text m_HighscoresNames;
+    public Text m_HighscoresScores;
     public Slider m_VolumeSlider;
     public Text m_VolumeSliderText;
     public Slider m_FOVSlider;
     public Text m_FOVSliderText;
     public float m_LevelTime;
-    private float m_DisplayTime;
+    public float m_DisplayTime;
     public Text m_TimerDisplay;
     public Text m_MessageText;
-    private bool m_isWaiting;
-    private int waitFor;
+    public bool m_GameOver = false;
     string currentDirectory;
-    public string m_SettingsFileName = "settings.txt";
-    public float[] m_SettingsValues = new float[2];
-    //private float waitFor;
+    public bool waiting = false;
+    public float waitFor;
+    public bool GameStateStartUp;
     //private bool wait = false;
     // Start is called before the first frame update
+    public enum GameState
+    {
+        Opening,
+        Start,
+        Playing,
+        GameOver,
+        Paused
+    };
+    public GameState m_GameState;
+    public GameState State { get { return m_GameState; } }
     void Start()
     {
         currentDirectory = Application.dataPath;
         Debug.Log("Directory - " + currentDirectory);
-        LoadSettings();
-        ChangeVolume();
-        ChangeFOV();
-        m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
-        VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
-        m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
-        foreach (GameObject gameObject in m_PauseMenu)
-        {
-            gameObject.gameObject.SetActive(false);
-        }
-        m_MessageText.gameObject.SetActive(false);
-        AudioListener.volume = m_VolumeSlider.value;
-        m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
-        m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
-        m_DisplayTime = m_LevelTime;
-        m_MessageText.text = "Ready...";
-        Time.timeScale = 0;
-        //waitFor = 3;
-        //m_isWaiting = true;
-        //StartCoroutine(WaitCommand(waitFor));
-        //WaitUntil waitUntil;
-        //waitUntil = m_isWaiting == false;
-        m_MessageText.text = "Go!";
-        //waitFor = 0.5f;
-        //StartCoroutine(WaitCommand(waitFor));
-        m_MessageText.gameObject.SetActive(false);
-        Time.timeScale = 1;
+        m_SettingEditor.LoadSettings(m_VolumeSlider, m_FOVSlider);
+        m_HighScoreEditor.LoadScores();
+        GameStateStartUp = true;
+        m_GameState = GameState.Start;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Tab) && m_isGamePaused == false)
+        switch (m_GameState)
         {
-            PauseGame();
+            //case GameState.Opening:
+                //break;
+            case GameState.Start:
+                while (GameStateStartUp == true)
+                {
+                    m_PlayerParts[0].gameObject.transform.position = new Vector3(0, -0.5f, 0);
+                    m_PlayerParts[0].gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    foreach (GameObject gameObject in m_InGameHUD)
+                    {
+                        gameObject.gameObject.SetActive(false);
+                    }
+                    m_PauseMenu[4].SetActive(false);
+                    foreach (GameObject gameObject in m_PauseMenu)
+                    {
+                        gameObject.gameObject.SetActive(false);
+                    }
+                    m_PauseMenu[0].gameObject.SetActive(true);
+                    m_PauseMenu[2].gameObject.SetActive(true);
+                    Time.timeScale = 0;
+                    m_isGamePaused = true;
+                    foreach (Component part in m_PlayerParts)
+                    {
+                        part.gameObject.SetActive(false);
+                    }
+                    m_DisplayTime = m_LevelTime;
+                    GameStateStartUp = false;
+                }
+                break;
+            case GameState.Playing:
+                while (GameStateStartUp == true)
+                {
+                    ChangeVolume();
+                    ChangeFOV();
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    Time.timeScale = 1;
+                    m_Score = 0;
+                    VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
+                    foreach (GameObject gameObject in m_InGameHUD)
+                    {
+                        gameObject.gameObject.SetActive(true);
+                    }
+                    foreach (GameObject gameObject in m_PauseMenu)
+                    {
+                        gameObject.gameObject.SetActive(false);
+                    }
+                    foreach (Component part in m_PlayerParts)
+                    {
+                        part.gameObject.SetActive(true);
+                    }
+                    //m_GameOver = false;
+                    if (m_NameInput.text != "")
+                    {
+                        m_PlayerName = m_NameInput.text;
+                    }
+                    else
+                    {
+                        m_PlayerName = "John Doe";
+                    }
+                    vacGun.m_LoadedGarbage = 0;
+                    m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
+                    m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
+                    m_MessageText.gameObject.SetActive(false);
+                    AudioListener.volume = m_VolumeSlider.value;
+                    m_VolumeSliderText.text = "Volume: " + Mathf.RoundToInt(m_VolumeSlider.value * 100).ToString() + "%";
+                    m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
+                    //m_MessageText.text = "Ready...";
+                    //Time.timeScale = 0;
+                    //waitFor = 3;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    //WaitUntil waitUntil = wait == true;
+                    //m_MessageText.text = "Go!";
+                    //StartCoroutine(Timer(m_DisplayTime, m_TimerDisplay));
+                    //waitFor = 0.5f;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    m_MessageText.gameObject.SetActive(false);
+                    //Time.timeScale = 1;
+                    GameStateStartUp = false;
+                }
+                if (Input.GetKeyUp(KeyCode.Escape) && m_isGamePaused == false)
+                {
+                    PauseGame();
+                }
+                UpdateTimer();
+                break;
+            case GameState.Paused:
+                break;
+            case GameState.GameOver:
+                while (GameStateStartUp == true)
+                {
+                    ItemSpawner itemSpawner = UnityEngine.Object.FindObjectOfType<ItemSpawner>();
+                    foreach (GameObject garbageball in itemSpawner.m_AllGarbage)
+                    {
+                        garbageball.SetActive(false);
+                    }
+                    itemSpawner.m_AllGarbage.RemoveRange(0, itemSpawner.m_AllGarbage.Count);
+                    m_MessageText.gameObject.SetActive(true);
+                    Debug.Log("Game Over");
+                    PauseGame();
+                    m_isGamePaused = false;
+                    m_PauseMenu[1].SetActive(false);
+                    m_PauseMenu[3].SetActive(true);
+                    m_MessageText.text = "Time's Up!";
+                    //waitFor = 2;
+                    //StartCoroutine(WaitCommand(waitFor));
+                    m_HighScoreEditor.AddScore(m_Score, m_PlayerName);
+                    highscoreEditor highscoreEditor = UnityEngine.Object.FindObjectOfType<highscoreEditor>();
+                    if (highscoreEditor.m_AddedToHighscores == true)
+                    {
+                        m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
+                    }
+                    else
+                    {
+                        m_MessageText.text = "Your final score was " + m_Score + ".";
+                    }
+                    m_HighScoreEditor.SaveScores();
+                    GameStateStartUp = false;
+                }
+                break;
         }
-        else if (Input.GetKeyUp(KeyCode.Tab) && m_isGamePaused == true)
-        {
-            ResumeGame();
-        }
-        UpdateTimer();
     }
     public void UpdateScoreText()
     {
@@ -86,23 +190,27 @@ public class GameManager : MonoBehaviour
     }
     public void QuitGame()
     {
+        m_HighScoreEditor.SaveScores();
+        m_SettingEditor.SaveSettings(m_VolumeSlider, m_FOVSlider);
         Debug.Log("Quiting... Loser!");
         Application.Quit();
     }
     public void PauseGame()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        foreach (GameObject gameObject in m_InGameHUD)
-        {
-            gameObject.gameObject.SetActive(false);
-        }
-        foreach (GameObject gameObject in m_PauseMenu)
-        {
-            gameObject.gameObject.SetActive(true);
-        }
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            foreach (GameObject gameObject in m_InGameHUD)
+            {
+                gameObject.gameObject.SetActive(false);
+            }
+            foreach (GameObject gameObject in m_PauseMenu)
+            {
+                gameObject.gameObject.SetActive(false);
+            }
+        m_PauseMenu[0].gameObject.SetActive(true);
+        m_PauseMenu[1].gameObject.SetActive(true);
         Time.timeScale = 0;
-        m_isGamePaused = true;
+            m_isGamePaused = true;
         foreach (Component part in m_PlayerParts)
         {
             part.gameObject.SetActive(false);
@@ -122,11 +230,11 @@ public class GameManager : MonoBehaviour
         }
         Time.timeScale = 1;
         m_isGamePaused = false;
-        foreach(Component part in m_PlayerParts)
+        foreach (Component part in m_PlayerParts)
         {
             part.gameObject.SetActive(true);
         }
-        SaveSettings();
+        m_SettingEditor.SaveSettings(m_VolumeSlider, m_FOVSlider);
     }
     public void ChangeVolume()
     {
@@ -138,92 +246,104 @@ public class GameManager : MonoBehaviour
         Camera.main.fieldOfView = ((m_FOVSlider.value * 100) + 30);
         m_FOVSliderText.text = "FOV: " + Mathf.RoundToInt((m_FOVSlider.value * 100) + 30).ToString();
     }
-    public void LoadSettings()
-    {
-        bool fileExists = File.Exists(currentDirectory + "\\" + m_SettingsFileName);
-        if (fileExists == true)
-        {
-            Debug.Log(m_SettingsFileName + " exists");
-        }
-        else
-        {
-            Debug.Log(m_SettingsFileName + " does not exist", this);
-            return;
-        }
-        StreamReader fileReader;
-        try
-        {
-            fileReader = new StreamReader(currentDirectory + "\\" + m_SettingsFileName);
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e.Message);
-            return;
-        }
-        m_SettingsValues = new float[m_SettingsValues.Length];
-        int SettingsValueCount = 0;
-        while (fileReader.Peek() != 0 && SettingsValueCount < m_SettingsValues.Length)
-        {
-            string fileLine = fileReader.ReadLine();
-            float readValue = -1;
-            bool didParse = float.TryParse(fileLine, out readValue);
-            if (didParse)
-            {
-                m_SettingsValues[SettingsValueCount] = readValue;
-            }
-            else
-            {
-                Debug.Log("INVALID SETTINGS VALUE @ " + SettingsValueCount + ", USING DEFAULT VALUE.", this);
-                m_SettingsValues[SettingsValueCount] = 0;
-            }
-            SettingsValueCount++;
-        }
-        fileReader.Close();
-        Debug.Log("Settings Loaded. Applying...");
-        m_VolumeSlider.value = m_SettingsValues[0];
-        m_FOVSlider.value = m_SettingsValues[1];
-    }
-    public void SaveSettings()
-    {
-        m_SettingsValues[0] = m_VolumeSlider.value;
-        m_SettingsValues[1] = m_FOVSlider.value;
-        StreamWriter fileWriter = new StreamWriter(currentDirectory + "\\" + m_SettingsFileName);
-        for (int i = 0; i < m_SettingsValues.Length; i++)
-        {
-            fileWriter.WriteLine(m_SettingsValues[i]);
-        }
-        fileWriter.Close();
-        Debug.Log("Settings Updated");
-    }
     public void UpdateTimer()
     {
         m_DisplayTime -= Time.deltaTime;
         int seconds = Mathf.RoundToInt(m_DisplayTime);
         m_TimerDisplay.text = string.Format("{0:D2}:{1:D2}", (seconds / 60), (seconds % 60));
-        if(m_DisplayTime <= 0)
+        if (m_DisplayTime <= 0)
         {
-            GameOver();
+            //while (m_GameOver == false)
+            //{
+            GameStateStartUp = true;
+                m_GameState = GameState.GameOver;
+                //m_GameOver = true;
+            //}
         }
+        
     }
-    public void GameOver()
+    /*IEnumerator Timer(float m_DisplayTime, Text m_TimerDisplay)
     {
-        m_MessageText.gameObject.SetActive(true);
-        Debug.Log("Game Over");
-        PauseGame();
-        m_isGamePaused = false;
-        m_PauseMenu[1].SetActive(false);
-        m_MessageText.text = "Time's Up!";
-        //waitFor = 2;
-        //StartCoroutine(WaitCommand(waitFor));
-        m_MessageText.text = "Your final score was " + m_Score + "!\nWell done!";
-
-    }
+        while (true)
+        {
+            m_DisplayTime -= Time.deltaTime;
+            int seconds = Mathf.RoundToInt(m_DisplayTime);
+            m_TimerDisplay.text = string.Format("{0:D2}:{1:D2}", (seconds / 60), (seconds % 60));
+            if (m_DisplayTime <= 0)
+            {
+                GameOver();
+            }
+        }
+    }*/
     IEnumerator WaitCommand(float waitFor)
     {
         Debug.Log("Waiting for " + waitFor + " seconds");
         yield return new WaitForSecondsRealtime(waitFor);
-        m_isWaiting = false;
+
         Debug.Log("Done waiting");
 
+    }
+    public void StartPlaying()
+    {
+        if (m_NameInput.text != "")
+        {
+            GameStateStartUp = true;
+            m_isGamePaused = false;
+            m_GameState = GameState.Playing;
+        }
+    }
+    public void RestartGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        foreach (GameObject gameObject in m_InGameHUD)
+        {
+            gameObject.gameObject.SetActive(true);
+        }
+        foreach (GameObject gameObject in m_PauseMenu)
+        {
+            gameObject.gameObject.SetActive(false);
+        }
+        Time.timeScale = 1;
+        m_isGamePaused = false;
+        foreach (Component part in m_PlayerParts)
+        {
+            part.gameObject.SetActive(true);
+        }
+        m_PlayerParts[0].gameObject.transform.position = new Vector3(0, -0.5f, 0);
+        m_PlayerParts[0].gameObject.transform.position = new Vector3(0, 0.4000001f, 0);
+        m_Score = 0;
+        m_ScoreDisplay.text = "Score:\n" + m_Score.ToString();
+        VacGun vacGun = UnityEngine.Object.FindObjectOfType<VacGun>();
+        vacGun.m_LoadedGarbage = 0;
+        m_GarbageLoadedDisplay.text = "Ammo:\n" + vacGun.m_LoadedGarbage.ToString();
+        m_DisplayTime = m_LevelTime;
+        m_GameState = GameState.Playing;
+    }
+    public void DisplayHighscores()
+    {
+        highscoreEditor highscoreEditor = UnityEngine.Object.FindObjectOfType<highscoreEditor>();
+        foreach (GameObject gameObject in m_PauseMenu)
+        {
+            gameObject.gameObject.SetActive(false);
+        }
+        m_PauseMenu[0].SetActive(true);
+        m_PauseMenu[4].SetActive(true);
+        int i = 0;
+        string HSNamesList = "";
+        string HSScoresList = "";
+        foreach (int score in highscoreEditor.m_Scores)
+        {
+            HSNamesList += highscoreEditor.m_ScoreNames[i] + "\n";
+            HSScoresList += highscoreEditor.m_Scores[i].ToString() + "\n";
+            i++;
+        }
+        m_HighscoresNames.text = HSNamesList;
+        m_HighscoresScores.text = HSScoresList;
+    }
+    public void ReturnToStart()
+    {
+        GameStateStartUp = true;
+        m_GameState = GameState.Start;
     }
 }
--- Assets/Scripts/SinMovement.cs
+++ Assets/Scripts/SinMovement.cs
@@ -4,67 +4,45 @@ using UnityEngine;
 
 public class SinMovement : MonoBehaviour
 {
-    public int m_Mode;
-    public bool m_LockSpeedSize = false;
+    public float size;
+    public float speed;
     private float x_pos;
-    public float x_size;
-    public float x_speed;
     private float y_pos;
-    public float y_size;
-    public float y_speed;
     private float z_pos;
-    public float z_size;
-    public float z_speed;
+    public string movingAxes;
     // Start is called before the first frame update
     void Start()
     {
-        switch (m_LockSpeedSize)
-        {
-            case true:
-                y_speed = x_speed;
-                z_speed = x_speed;
-                y_size = x_size;
-                z_size = x_size;
-                break;
-            default:
-                break;
-        }
         x_pos = transform.position.x;
         y_pos = transform.position.y;
         z_pos = transform.position.z;
-        Debug.Log(x_pos);
-        Debug.Log(y_pos);
-        Debug.Log(z_pos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (m_Mode)
+        switch (movingAxes)
         {
-            case 0: //x
-                transform.position = new Vector3(x_pos + Mathf.Sin(Time.time * x_speed) * x_size, y_pos, z_pos);
+            case "x":
+                transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, y_pos, z_pos);
                 break;
-            case 1: //y
-                transform.position = new Vector3(x_pos, y_pos + Mathf.Sin(Time.time * y_speed) * y_size, z_pos);
+            case "y":
+                transform.position = new Vector3(x_pos, Mathf.Sin(Time.time * speed) * size, z_pos);
                 break;
-            case 2: //z
-                transform.position = new Vector3(x_pos, y_pos, z_pos + Mathf.Sin(Time.time * z_speed) * z_size);
+            case "z":
+                transform.position = new Vector3(x_pos, y_pos, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case 3: //xy
-                transform.position = new Vector3(x_pos + Mathf.Sin(Time.time * x_speed) * x_size, y_pos + Mathf.Sin(Time.time * y_speed) * y_size, z_pos);
+            case "xy":
+                transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size, z_pos);
                 break;
-            case 4: //xz
-                transform.position = new Vector3(x_pos + Mathf.Sin(Time.time * x_speed) * x_size, y_pos, z_pos + Mathf.Sin(Time.time * z_speed) * z_size);
+            case "xz":
+                transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, y_pos, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case 5: //yz
-                transform.position = new Vector3(x_pos, y_pos + Mathf.Sin(Time.time * y_speed) * y_size, z_pos + Mathf.Sin(Time.time * z_speed) * z_size);
+            case "yz":
+                transform.position = new Vector3(x_pos, Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case 6: //xyz
-                transform.position = new Vector3(x_pos + Mathf.Sin(Time.time * x_speed) * x_size, y_pos + Mathf.Sin(Time.time * y_speed) * y_size, z_pos + Mathf.Sin(Time.time * z_speed) * z_size);
-                break;
-            default:
-                transform.position = new Vector3(x_pos, y_pos, z_pos);
+            case "xyz":
+                transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size);
                 break;
         }
     }
