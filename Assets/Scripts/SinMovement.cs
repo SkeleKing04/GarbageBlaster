cdaa025f3ccf48be5508151ea2da4561d7932227
Jackson Lucas <jjlucas04@bigpond.com>
2021-08-31 08:41:40 +0930

    Update SinMovement.cs

--- Assets/Scripts/SinMovement.cs
+++ Assets/Scripts/SinMovement.cs
@@ -9,7 +9,17 @@ public class SinMovement : MonoBehaviour
     private float x_pos;
     private float y_pos;
     private float z_pos;
-    public string movingAxes;
+    public enum e_MoveableAxes
+    {
+        x,
+        y,
+        z,
+        xy,
+        xz,
+        yz,
+        xyz
+    };
+    public e_MoveableAxes m_MoveableAxes;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,27 +31,27 @@ public class SinMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (movingAxes)
+        switch (m_MoveableAxes)
         {
-            case "x":
+            case e_MoveableAxes.x:
                 transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, y_pos, z_pos);
                 break;
-            case "y":
+            case e_MoveableAxes.y:
                 transform.position = new Vector3(x_pos, Mathf.Sin(Time.time * speed) * size, z_pos);
                 break;
-            case "z":
+            case e_MoveableAxes.z:
                 transform.position = new Vector3(x_pos, y_pos, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case "xy":
+            case e_MoveableAxes.xy:
                 transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size, z_pos);
                 break;
-            case "xz":
+            case e_MoveableAxes.xz:
                 transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, y_pos, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case "yz":
+            case e_MoveableAxes.yz:
                 transform.position = new Vector3(x_pos, Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size);
                 break;
-            case "xyz":
+            case e_MoveableAxes.xyz:
                 transform.position = new Vector3(Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size, Mathf.Sin(Time.time * speed) * size);
                 break;
         }
